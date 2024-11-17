b932bf552ebe5b1955fdcb94fd4ccc773b8127f5
hek1412 <hek1412@gmail.com>
2024-11-17 10:01:57 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ def print_hello():
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 7, 19),
+    'start_date': datetime(2024, 11, 17),
     'email_on_failure': False,
     'email_on_retry': False,
 }

439f51e55c0d6b93270b65a0b53185f68be5b81b
hek1412 <hek1412@gmail.com>
2024-11-17 11:06:02 +0300

    Create pascal.py

---
+++ pascal.py
@@ -0,0 +1,53 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+def generate_pascals_triangle(n):
+    triangle = []
+    for row_num in range(n):
+        # Начинаем каждую строку с 1
+        row = [1] * (row_num + 1)
+        # Заполняем значения строки, кроме первого и последнего
+        for j in range(1, row_num):
+            row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+        triangle.append(row)
+    return triangle
+
+def print_pascals_triangle(triangle):
+    max_width = len(" ".join(map(str, triangle[-1])))
+    for row in triangle:
+        row_str = " ".join(map(str, row))
+        print(row_str.center(max_width))
+# Количество уровней треугольника Паскаля
+levels = 10
+triangle = generate_pascals_triangle(levels)
+print_pascals_triangle(triangle)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 17),
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+dag = DAG('pascal', default_args=default_args, schedule_interval='@daily')
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+python_task = PythonOperator(
+    task_id='print_pascals_triangle',
+    python_callable=print_pascals_triangle,
+    dag=dag,
+)
+
+start >> python_task >> end

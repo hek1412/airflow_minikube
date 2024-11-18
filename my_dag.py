5d515c98f69ad090ba54534a88e6eb4656705fc5
hek1412 <hek1412@gmail.com>
2024-11-18 13:57:01 +0300

    Update my_dag.py

--- my_dag.py
+++ my_dag.py
@@ -4,32 +4,23 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import subprocess
 
-# Определяем Python-функцию, которую будем вызывать предназначена для получения и отображения времени безотказной работы системы (uptime)
-# def print_uptime():
-#     try:
-#         result = subprocess.run(['uptime', '-p'], capture_output=True, text=True)
-#         uptime = result.stdout.strip()
-#         # Форматирование
-#         formatted_uptime = '. '.join(uptime.split(', '))
-#         print(f"\nТекущее время безотказной работы составляет:\n... {formatted_uptime} ...\n")
-        
-#     except Exception as e:
-#         print(f"Ошибка: {str(e)}")
+# Определяем Python-функцию, которую будем вызывать для получения и отображения времени безотказной работы системы
 def print_uptime():
     try:
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = float(f.readline().split()[0])
             uptime_minutes = int(uptime_seconds / 60)
-            formatted_uptime = f"{uptime_minutes} минут"  # Можно добавить более сложное форматирование
+            formatted_uptime = f"{uptime_minutes} минут"  
             print(f"\nТекущее время безотказной работы составляет:\n... {formatted_uptime} ...\n")
         
     except Exception as e:
         print(f"Ошибка: {str(e)}")
+
 # Задаем параметры DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 11, 18, 9, 45, 0),  # Убедитесь, что дата не в будущем
+    'start_date': datetime(2023, 11, 18, 9, 45, 0),  
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
 }
@@ -46,7 +37,7 @@ with DAG(
     # Создаем задачу с использованием BashOperator
     bash_task = BashOperator(
         task_id='bash_task',
-        bash_command='echo "Текущее время: $(date)"',  # Правильный bash синтаксис
+        bash_command='echo "Текущее время: $(date)"', 
     )
 
     # Создаем задачу с использованием PythonOperator
@@ -55,5 +46,4 @@ with DAG(
         python_callable=print_uptime,
     )
 
-    # Определяем последовательность выполнения задач
     bash_task >> uptime_task
